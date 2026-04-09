2c1be9a7ecaa4d4fd1de5a3c268ee7a2257f122d
Vanshika <vanshikaraje010@gmail.com>
2026-04-09 09:46:21 +0530

    Time: 1127 ms (87.31%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,5 +1,4 @@
 # Write your MySQL query statement below
-select product_name,s.year,s.price
-from Sales s
-join Product p
+select p.product_name,s.year,s.price from Product p
+inner join Sales s
 on p.product_id = s.product_id;
