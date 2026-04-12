97903da1ef05b99b762468650839e7eb694c61de
Vanshika <vanshikaraje010@gmail.com>
2026-04-12 19:12:56 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (72.22%) - LeetSync

---
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countGoodSubstrings(string s) {
+
+        if (s.length() < 3) return 0;  // 🔥 important
+
+        int count = 0;
+
+        for (int i = 0; i < s.length() - 2; i++) {
+
+            if (s[i] != s[i+1] &&
+                s[i] != s[i+2] &&
+                s[i+1] != s[i+2]) {
+
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
